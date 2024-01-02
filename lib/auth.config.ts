01d24093c98fb5f3d53b02082c15a4a06c13e699
arfianarif <arfian.prasetyo@baleomol.dev>
2024-01-02 15:58:59 +0700

    feat(auth) : add github and google login without linkAccount

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -1,3 +1,4 @@
+import Google from "next-auth/providers/google"
 import GitHub from "next-auth/providers/github"
 import Credentials from "next-auth/providers/credentials"
 import { LoginSchema } from "@/schemas"
@@ -8,6 +9,14 @@ import { getUserByEmail } from "@/data/user"
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials)
